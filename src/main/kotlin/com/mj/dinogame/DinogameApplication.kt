522b40caf33b9d800c4fe15d9a3f1d1b3cc96587
Meike <92315934+mjung2605@users.noreply.github.com>
2024-03-24 20:33:19 +0100

    Initial commit - basic game logic (jump, game over)

---
+++ src/main/kotlin/com/mj/dinogame/DinogameApplication.kt
@@ -0,0 +1,11 @@
+package com.mj.dinogame
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DinogameApplication
+
+fun main(args: Array<String>) {
+    runApplication<DinogameApplication>(*args)
+}
---
+++ src/test/kotlin/com/mj/dinogame/DinogameApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.mj.dinogame
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DinogameApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
